35abb5c846c53bec3c683816ab5a019e5c442021
krispenner <kris@wealthly.ca>
2018-11-27 14:23:54 -0800

    ASP.NET Core MVC project with Identity using Int64 for keys, EF, and Multi-Tenancy support added.

---
+++ src/Int64AspNetIdentityAndEFCore/Startup.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MultiTenancyServer.Samples.AspNetIdentityAndEFCore.Data;
+using MultiTenancyServer.Samples.AspNetIdentityAndEFCore.Models;
+using MultiTenancyServer.Samples.AspNetIdentityAndEFCore.Services;
+
+namespace MultiTenancyServer.Samples.AspNetIdentityAndEFCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole<long>>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add Multi-Tenancy services.
+            services.AddMultiTenancy<ApplicationTenant, long>()
+                .AddRequestParsers(parsers =>
+                {
+                    // To test a domain parser locally, add a similar line 
+                    // to your hosts file for each tenant you want to test
+                    // For Windows: C:\Windows\System32\drivers\etc\hosts
+                    // 127.0.0.1	tenant1.tenants.local
+                    //parsers.AddSubdomainParser(".tenants.local");
+
+                    parsers.AddChildPathParser("/tenants/");
+                })
+                .AddEntityFrameworkStore<ApplicationDbContext, ApplicationTenant, long>();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMultiTenancy<ApplicationTenant>();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    // if using a PathParser, you will need to adjust this to accomodate the tenant paths
+                    template: "tenants/{_tenant_placeholder_}/{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
